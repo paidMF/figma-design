9da12c28e7e7f11672a6e0df3017b1f27af17b67
Olufemi Olaosebikan <olufemiolaosebikan.t@gmail.com>
2023-10-11 02:54:01 -0700

    summary

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Searchbar from '../search-bar/Searchbar'
+import "./header.css"
+
+
+import { HiHome, HiViewGrid } from 'react-icons/hi'
+import "../sidebar/Sidebar.css"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+   
+    // <section className="header-section">
+
+    //    <div className='flexCenter logo'>
+    //         Logo
+    //    </div>
+
+
+    //     <div className=" flexCenter header-container">
+    //         <Searchbar />
+    //     </div>
+
+        
+
+    // </section>
+  )
+}
+
+export default Header
